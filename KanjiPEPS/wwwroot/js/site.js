51059f0416c69f7fed1998b04c1e4c837277971b
Lucas Lebihan <htfilia@gmail.com>
2020-01-13 03:45:45 +0100

    Continuité web theme et ajout js pour commencer les graphs

--- KanjiPEPS/wwwroot/js/site.js
+++ KanjiPEPS/wwwroot/js/site.js
@@ -2,3 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Panel on Sideline Navbar
+
+window.addEventListener('load', main, false);
+
+function activateLink() {
+    var parent = $(this).parent();
+    if (!parent[0].classList.contains("active")) {
+        parent.parent().children().each(function () {
+            $(this).removeClass("active");
+        });
+        parent.addClass("active");
+    }
+}
+
+// Draw graph of kanji's price on sidebars
+function createGraph() {
+    //TODO
+}
+
+// Print current date on index page
+function printDate() {
+    n = new Date();
+    y = n.getFullYear();
+    m = n.getMonth();
+    d = n.getDate();
+    document.getElementById("date").innerHTML = ("0" + d).slice(-2) + "/" + ("0" + (m + 1)).slice(-2) + "/" + y;
+}
+
+function main() {
+    // Event on click on link on sidebars
+    $("body").on('click', '.nav-link', activateLink);
+    // Draw graph of kanji's price over time
+    createGraph();
+    // Print current date on index page
+    printDate();
+}
