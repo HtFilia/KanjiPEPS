04197d4bd063e9ebb19c28910c664cfce2113dc3
Amine <amine.zarbi@grenoble-inp.org>
2020-01-18 16:59:45 +0100

    Integration of C# code, Reading Data and storing in Data Feed Calibration (WRE not working yet, licence problem) Pricing of Kanji Based on historical data, need to debug MC

--- Pricer/Computations.cpp
+++ Pricer/Computations.cpp
@@ -1,11 +1,11 @@
 #include "Computations.hpp"
 #include <iostream>
+#include <time.h>
 #include "pnl/pnl_random.h"
 #include "MonteCarlo.hpp"
 #include "BlackScholesModel.hpp"
 #include "VanillaCall.hpp"
 #include "PerformanceOption.hpp"
-#include <ctime>
 #include "pnl/pnl_vector.h"
 using namespace std;
 
@@ -20,7 +20,7 @@ void Computations::callMC(double &ic, double &prix, int nb_samples, double T,
 
 	Option *vanilla_call = new VanillaCall(T, 1, 1, K);
 	PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
-	pnl_rng_sseed(rng, 0);
+	pnl_rng_sseed(rng, time(NULL));
 
 	MonteCarlo *mc = new MonteCarlo(model, vanilla_call, rng, T, nb_samples, 0.05);
 
@@ -34,7 +34,7 @@ void Computations::calleuro(double &ic, double &prix, int nb_samples, double T,
 	double sum = 0;
 	double var = 0;
 	PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
-	pnl_rng_sseed(rng, 0);
+	pnl_rng_sseed(rng, time(NULL));
 	double payoff;
 	for (int i = 0; i < nb_samples; i++)
 	{
@@ -49,9 +49,8 @@ void Computations::calleuro(double &ic, double &prix, int nb_samples, double T,
 	ic = 1.96 * sqrt(var / nb_samples);
 	pnl_rng_free(&rng);
 }
-
-void Computations::performance_price_hedge(double &ic, double &prix, double ic_deltas[], double deltas[], int nb_samples, double T,
-	double S0_[], double K, double sigma_[], double correlation, double r) {
+void  Computations::performance_price_hedge(double &ic, double &prix, double ic_deltas[], double deltas[], int nb_samples, double T,
+	double S0_[], double sigma_[], double correlation, double r) {
 	int size = 3;
 
 	PnlVect* ic_delta = pnl_vect_create_from_ptr(size, ic_deltas);
@@ -60,14 +59,71 @@ void Computations::performance_price_hedge(double &ic, double &prix, double ic_d
 	PnlVect* sigma = pnl_vect_create_from_ptr(size, sigma_);
 	PnlVect* trend_vec = pnl_vect_create_from_double(1, r);
 	BlackScholesModel *model = new BlackScholesModel(size, r, correlation, sigma, S0, trend_vec);
-	PnlVect* payOffCoeffs = pnl_vect_create_from_scalar(3,1/3);
-	int timesteps = 100; //à revoir
+	PnlVect* payOffCoeffs = pnl_vect_create_from_scalar(3, 1.0 / 3.0);
+	int timesteps = 16; //voir boucle dans perf payoff 
 	Option *perf_option = new PerformanceOption(T, timesteps, size, payOffCoeffs);
 	PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
 	pnl_rng_sseed(rng, time(NULL));
 
-	MonteCarlo *mc = new MonteCarlo(model, perf_option, rng, T/nb_samples, nb_samples, 0.05);
-	PnlMat *past = pnl_mat_create_from_double(1, 3, 0);
+	MonteCarlo *mc = new MonteCarlo(model, perf_option, rng, T / nb_samples, nb_samples, 0.1);
+	PnlMat *past = pnl_mat_create_from_double(1, 3, S0_[0]);
 	pnl_mat_set_row(past, S0, 0);
 	mc->price_and_delta(past, 0, prix, ic, delta, ic_delta);
+	pnl_vect_print(delta);
+	for (int i = 0; i < delta->size; i++) {
+		deltas[i] = pnl_vect_get(delta, i);
+		//std::cout << deltas[i] << std::endl;
+		//std::cout << pnl_vect_get(delta, i) << std::endl;
+
+	}
+}
+
+void Computations::performance_price_hedge_t(double &ic, double &prix, double ic_deltas[], double deltas[], int nb_samples, double T,
+	double t, double path_[], double nb_dates, double sigma_[], double correlation, double r)
+{
+	int size = 3;
+	PnlVect* ic_delta = pnl_vect_create_from_ptr(size, ic_deltas);
+	PnlVect* delta = pnl_vect_create_from_ptr(size, deltas);
+	PnlMat *path = pnl_mat_create_from_ptr(nb_dates, size, path_);
+	PnlMat *past = pnl_mat_create(nb_dates, size);
+	PnlVect* S0 = pnl_vect_create(3);
+	pnl_mat_get_row(S0, path, 0);
+	PnlVect* sigma = pnl_vect_create_from_ptr(size, sigma_);
+	PnlVect* trend_vec = pnl_vect_create_from_double(1, r);
+	BlackScholesModel *model = new BlackScholesModel(size, r, correlation, sigma, S0, trend_vec);
+	PnlVect* payOffCoeffs = pnl_vect_create_from_scalar(3, 1.0 / 3.0);
+	int timesteps = 16; //voir boucle dans perf payoff
+	model->getPast(past, path, t, timesteps, T);
+
+	Option *perf_option = new PerformanceOption(T, timesteps, size, payOffCoeffs);
+	PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
+	pnl_rng_sseed(rng, time(NULL));
+
+	MonteCarlo *mc = new MonteCarlo(model, perf_option, rng, T / nb_samples, nb_samples, 0.07);
+	mc->price_and_delta(past, t, prix, ic, delta, ic_delta);
+	for (int i = 0; i < delta->size; i++) {
+		deltas[i] = pnl_vect_get(delta, i);
+
+	}
+}
+void Computations::simul_market(double path_[], double t, double maturity, int nbHedging_dates, double s0_[], double trends_[], double sigmas_[], double correlation, double r)
+{
+	int size_path = (t * nbHedging_dates) / maturity;
+	int size = 3;
+	PnlMat* path = pnl_mat_create(size_path, size);
+	PnlVect* s0 = pnl_vect_create_from_ptr(size, s0_);
+	PnlVect* sigma = pnl_vect_create_from_ptr(size, sigmas_);
+	PnlVect* trend_vec = pnl_vect_create_from_ptr(size, trends_);
+	BlackScholesModel *model = new BlackScholesModel(size, r, correlation, sigma, s0, trend_vec);
+
+	PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
+	pnl_rng_sseed(rng, time(NULL));
+
+	model->simul_market(path, maturity, nbHedging_dates, rng);
+	for (int index_row = 0; index_row < path->m; index_row++) {
+		for (int index_col = 0; index_col < path->n; index_col++) {
+			path_[index_row * path->n + index_col] = pnl_mat_get(path, index_row, index_col);
+		}
+
+	}
 }
---
+++ PricingKanji/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PricingKanji.Model;
+using PricingLibrary.Utilities.MarketDataFeed;
+
+namespace PricingKanji
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            DataReader reader = new DataReader();
+            List<DataFeed> data = reader.ReadData();
+            Pricing pricer = new Pricing();
+            List<double> prices = pricer.PriceKanji(data, 0.07);
+            Console.WriteLine("hi");
+        }
+    }
+}
