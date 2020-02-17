f86e358951893bdd6fb9d2a21c18d09f7e5ecdb2
BOUTZATE <zouhair.boutzate@grenoble-inp.org>
2020-02-17 19:49:14 +0100

    memory debug

--- Pricer/VanillaCall.cpp
+++ Pricer/VanillaCall.cpp
@@ -15,9 +15,7 @@ double VanillaCall::payoff(const PnlMat *path, double past_payoff, int past_len)
 }
 
 double VanillaCall::payoff(const PnlMat *path) {
-	PnlVect *vect = pnl_vect_create(path->m);
-	pnl_mat_get_col(vect, path, 0);
-	double S_T = pnl_vect_get(vect, path->m-1);
+	double S_T = pnl_mat_get(path, path->m - 1, 0);
 	return S_T > strike_ ? S_T - strike_ : 0;
 
 }
