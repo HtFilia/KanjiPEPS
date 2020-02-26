55436b3fbb7c496a41581e9fa7a66a679255d504
anas-mellouki <anas.mellouki@grenoble-inp.org>
2020-02-26 21:11:25 +0100

    kanji hedging results (20h)

--- PricerValidation/KanjiValidation.cpp
+++ PricerValidation/KanjiValidation.cpp
@@ -2,7 +2,7 @@
 
 void validate_kanji(PnlRng* rng) {
 	int n_time_steps = 16;
-	double T = 1;
+	double T = 4;
 	double r = 0.0001;
 	double spot_ = 100;
 	double sigma_ = 0.2;
@@ -18,16 +18,15 @@ void validate_kanji(PnlRng* rng) {
 	double gamma = -1.0 / 4.0;
 	double epsilon_n = epsilon * pow(n_samples, -gamma);
 	MonteCarlo* mc = new MonteCarlo(model, kanji, rng, T / n_time_steps, n_samples, 0.0001);
-	int M = 48;
+	int M = 1440; //4 ans * 360j
 	int H = M;
 	int n_scenarios = 50;
 	PnlMat* simulated_path = pnl_mat_create(M+1, 1);
 	//validate_price_kanji(simulated_path, model, mc, rng);
 	//validate_delta_kanji(simulated_path, model, mc, rng);
 	//validate_mean_error_kanji(mc, model, rng, M, H, n_scenarios);
-
-	int n_freqs = 1;
-	const double *freqs_ptr = new double[n_freqs] {1};
+	int n_freqs = 3;
+	const double *freqs_ptr = new double[n_freqs] {360, 180, 90};
 	PnlVect* freqs = pnl_vect_create_from_ptr(n_freqs, freqs_ptr);
 	histogram_errors_kanji(mc, model, rng, M, freqs, n_scenarios);
 	//pnl_mat_free(&simulated_path);
@@ -36,10 +35,10 @@ void validate_kanji(PnlRng* rng) {
 void histogram_errors_kanji(MonteCarlo* mc, BlackScholesModel* model, PnlRng* rng, int M, PnlVect* freqs, int scenarios) {
 	double r = model->r_;
 	KanjiOption* kanji = (KanjiOption*)mc->opt_; 
-	PnlMat* past = pnl_mat_create(1, 1);
+	PnlMat* past = pnl_mat_create(1, 3);
 	int n_time_steps = kanji->nbTimeSteps_;
 	double T = kanji->T_;
-	PnlMat* simulated_path = pnl_mat_create(1, 1);
+	PnlMat* simulated_path = pnl_mat_create(M+1, model->size_);
 	model->simul_market(simulated_path, T, M, rng);
 
 	Hedge hedge(mc);
@@ -55,7 +54,6 @@ void histogram_errors_kanji(MonteCarlo* mc, BlackScholesModel* model, PnlRng* rn
 		myfile.open(filename);
 		for (int i = 0; i < scenarios; i++) {
 			model->simul_market(simulated_path, T, M, rng);
-			pnl_mat_print(simulated_path);
 			hedge.PnLfreq(simulated_path, n_time_steps + 1, freq, portfolio_values, option_values, error); // amine
 			std::cout << error << std::endl;
 			myfile << error << ";" << std::endl;
