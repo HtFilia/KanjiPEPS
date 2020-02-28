813950c8a270b8859b00eacc4e5a78c7a66dc036
Lucas Lebihan <htfilia@gmail.com>
2020-02-28 19:06:40 +0100

    Conversion du site en ASP.NET Framework OK et début intégration pricer + wrapper dedans

---
+++ WebComponent/Models/PriceFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebComponent.Models
+{
+    public class PriceFormModel
+    {
+        public int SampleNb { get; set; }
+        public double Maturity { get; set; }
+        public double InitialPrice { get; set; }
+        public double Strike { get; set; }
+        public double Volatility { get; set; }
+        public double RiskFreeRate { get; set; }
+    }
+}
