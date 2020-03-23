cc24d2440534900b0d140825af2fed33f3e3f2df
anas-mellouki <anas.mellouki@grenoble-inp.org>
2020-03-23 12:34:33 +0100

    added exchange rates data

--- PricingKanji/Model/DataReader.cs
+++ PricingKanji/Model/DataReader.cs
@@ -10,7 +10,7 @@ using System.Globalization;
 
 namespace PricingKanji.Model
 {
-    public class DataReader
+    class DataReader
     {
         public List<DataFeed> ReadData()
         {
@@ -18,38 +18,70 @@ namespace PricingKanji.Model
             IndexValue EuroValues = ParseFile("ESTX 50");
             IndexValue SNPValues = ParseFile("S&P 500");
             IndexValue HSIValues = ParseFile("HANG SENG INDEX");
-
-            foreach(DateTime date in EuroValues.cotations.Keys)
+            foreach (DateTime date in EuroValues.cotations.Keys)
             {
-                if(SNPValues.cotations.ContainsKey(date) && HSIValues.cotations.ContainsKey(date)){
+                if (SNPValues.cotations.ContainsKey(date) && HSIValues.cotations.ContainsKey(date))
+                {
                     Dictionary<string, decimal> PriceList = new Dictionary<string, decimal>();
                     DataFeed feed = new DataFeed(date, PriceList);
                     PriceList.Add("ESTX 50", EuroValues.cotations[date]);
                     PriceList.Add("S&P 500", SNPValues.cotations[date]);
                     PriceList.Add("HANG SENG INDEX", HSIValues.cotations[date]);
-
-                    
                     feeds.Add(feed);
                 }
-
-
             }
-            
             return feeds;
         }
 
-        IndexValue ParseFile(String Name)
+        public List<DataFeed> ReadDataFX(decimal r_usd, decimal r_hkd)
+        {
+            List<DataFeed> feeds = new List<DataFeed>();
+            IndexValue EuroValues = ParseFile("ESTX 50");
+            IndexValue SNPValues = ParseFile("S&P 500");
+            IndexValue HSIValues = ParseFile("HANG SENG INDEX");
+            IndexValue EURUSDValues = ParseFile("EURUSD");
+            IndexValue EURHKDValues = ParseFile("EURHKD");
+            double timeSpan = 1.0 / 252.0;
+            double t_counter = 0;
+            foreach (DateTime date in EuroValues.cotations.Keys)
+            {
+
+                if (SNPValues.cotations.ContainsKey(date) && HSIValues.cotations.ContainsKey(date) && EURUSDValues.cotations.ContainsKey(date) && EURHKDValues.cotations.ContainsKey(date))
+                {
+                    Dictionary<string, decimal> PriceList = new Dictionary<string, decimal>();
+                    DataFeed feed = new DataFeed(date, PriceList);
+                    PriceList.Add("ESTX 50", EuroValues.cotations[date]);
+                    PriceList.Add("S&P 500", SNPValues.cotations[date] / EURUSDValues.cotations[date]);
+                    PriceList.Add("USD", (decimal)Math.Exp((double)r_usd * t_counter) / EURUSDValues.cotations[date]);
+                    PriceList.Add("HANG SENG INDEX", HSIValues.cotations[date] / EURHKDValues.cotations[date]);
+                    PriceList.Add("HDK", (decimal)Math.Exp((double)r_hkd * t_counter) / EURHKDValues.cotations[date]);
+                    feeds.Add(feed);
+                }
+                t_counter += timeSpan;
+            }
+
+            return feeds;
+        }
+
+        public IndexValue ParseFile(String Name)
         {
             String Path = "";
-            switch(Name){
+            switch (Name)
+            {
                 case "ESTX 50":
-                    Path = "../../../../eurostoxx.csv";
+                    Path = "../../../../MarketData/eurostoxx.csv";
                     break;
                 case "S&P 500":
-                    Path = "../../../../snp500.csv";
+                    Path = "../../../../MarketData/snp500.csv";
                     break;
                 case "HANG SENG INDEX":
-                    Path = "../../../../hsi.csv";
+                    Path = "../../../../MarketData/hsi.csv";
+                    break;
+                case "EURUSD":
+                    Path = "../../../../MarketData/EURUSD.csv";
+                    break;
+                case "EURHKD":
+                    Path = "../../../../MarketData/EURHKD.csv";
                     break;
             }
 
@@ -61,7 +93,7 @@ namespace PricingKanji.Model
             sr.ReadLine();
             while ((line = sr.ReadLine()) != null)
             {
-                
+
                 index_value.AddCotation(line.Split(','));
             }
             sr.Close();
