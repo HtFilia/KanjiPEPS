3a5027ef8552e904eb18127cb17c42e5689c72ee
Idriss Afra <idriss.afra@grenoble-inp.org>
2020-03-24 19:48:58 +0100

    message d'erreur pour le userDate !

--- WebComponent/Controllers/HomeController.cs
+++ WebComponent/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebComponent.Controllers
 {
     public class HomeController : Controller
     {
-        public static DateTime userDate = DateTime.Today;
+        public static DateTime userDate = new DateTime(2020, 1, 16);
 
         public static Hedging hedging = new Hedging(80, 1, userDate);
 
@@ -24,135 +24,28 @@ namespace WebComponent.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            KanjiQuot();
-            KanjiMens();
-            KanjiHebdo();
+
             ViewBag.UserDate = HomeController.userDate;
             ViewBag.Posted = false;
-            /*if (Request.HttpMethod == "GET")
-            {
-                WrapperClass wrapper = new WrapperClass();
-                wrapper.getPriceDeltaPerft(2, 0, new double[] { 100, 100, 100 }, 16, new double[] { 0.2, 0.2, 0.2 }, new double[] { 1, 0.1, 0.1, 0.1, 1, 0.1, 0.1, 0.1, 1}, 0.07);
-                ViewBag.Price = wrapper.getPrice();
-            }
-            
-            if (Request.HttpMethod == "POST")
-            {
-                int SampleNb = Convert.ToInt32(Request.Form["SampleNb"]);
-                double Maturity = Convert.ToDouble(Request.Form["Maturity"]);
-                double InitialPrice = Convert.ToDouble(Request.Form["InitialPrice"]);
-                double Strike = Convert.ToDouble(Request.Form["Strike"]);
-                double Volatility = Convert.ToDouble(Request.Form["Volatility"]);
-                double RiskFreeRate = Convert.ToDouble(Request.Form["RiskFreeRate"]);
-                WrapperClass wrapper = new WrapperClass();
-                wrapper.getPriceCallEuro(Maturity, InitialPrice, Strike, Volatility, RiskFreeRate);
-                ViewBag.Price = wrapper.getPrice();
-            }*/
             return View();
         }
 
         [HttpPost]
         public ActionResult Index(DateFormModel requestForm)
         {
-            KanjiQuot();
-            KanjiMens();
-            KanjiHebdo();
+
             ViewBag.Posted = true;
-            userDate = requestForm.DesiredDate;
-            ViewBag.UserDate = requestForm.DesiredDate;
+            if (requestForm.DesiredDate > new DateTime(2020, 1, 16) || requestForm.DesiredDate < new DateTime(2012, 1, 1))
+            {
+                ViewBag.MessageEr = "La date devra être entre l'année 2012 et le 16 Janvier de l'année 2020.";
+            }
+            else
+            {
+                userDate = requestForm.DesiredDate;
+                ViewBag.UserDate = requestForm.DesiredDate;
+            }
             return View();
         }
-
-
-        // On test avec le cours Stoxx50 pour le moment, à changer dès qu'on a le csv des prix !!!!!!!!!!!!
-
-        private void KanjiQuot()
-        {
-            List<DataPoint> dataPoints = new List<DataPoint>();
-            using (var reader = new StreamReader(path.Chemin + @"\WebComponent\Content\csv\Kanji.csv"))
-            {
-                NumberFormatInfo provider = new NumberFormatInfo();
-                provider.NumberDecimalSeparator = ".";
-
-                int compteur = 0;
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-
-                    if (compteur > 0)
-                    {
-                        var values = line.Split(';');
-                        dataPoints.Add(new DataPoint(values[0], Convert.ToDouble(values[1], provider)));
-                    }
-                    compteur++;
-                }
-            }
-
-
-            ViewBag.KanjiQuot = JsonConvert.SerializeObject(dataPoints);
-        }
-
-        private void KanjiMens()
-        {
-
-            List<DataPoint> dataPoints = new List<DataPoint>();
-            using (var reader = new StreamReader(path.Chemin + @"\WebComponent\Content\csv\Kanji.csv"))
-            {
-                NumberFormatInfo provider = new NumberFormatInfo();
-                provider.NumberDecimalSeparator = ".";
-
-                int compteur = 0;
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-
-                    if (compteur > 0)
-                    {
-                        if ((compteur-1) % 30 == 0)
-                        {
-                            var values = line.Split(';');
-                            dataPoints.Add(new DataPoint(values[0], Convert.ToDouble(values[1], provider)));
-                        }
-                    }
-                    compteur++;
-                }
-            }
-
-
-            ViewBag.KanjiMens = JsonConvert.SerializeObject(dataPoints);
-        }
-
-        private void KanjiHebdo()
-        {
-
-            List<DataPoint> dataPoints = new List<DataPoint>();
-            using (var reader = new StreamReader(path.Chemin + @"\WebComponent\Content\csv\Kanji.csv"))
-            {
-                NumberFormatInfo provider = new NumberFormatInfo
-                {
-                    NumberDecimalSeparator = "."
-                };
-
-                int compteur = 0;
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    if (compteur > 0)
-                    {
-                        if ((compteur-1) % 7 == 0)
-                        {
-                            var values = line.Split(';');
-                            dataPoints.Add(new DataPoint(values[0], Convert.ToDouble(values[1], provider)));
-                        }
-                    }
-                    compteur++;
-                }
-            }
-
-
-            ViewBag.KanjiHebdo = JsonConvert.SerializeObject(dataPoints);
-        }
-
 
 
     }
--- WebComponent/Utilities/Path.cs
+++ WebComponent/Utilities/Path.cs
@@ -11,7 +11,7 @@ namespace WebComponent.Utilities
 
         public Path()
         {
-            this.Chemin = @"C:\Users\Fibo\source\repos\HtFilia\KanjiPEPS";
+            this.Chemin = @"C:\Users\Idriss Afra\source\repos\KanjiPEPS2";
         }
     }
 }
