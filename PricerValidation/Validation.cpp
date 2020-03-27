6d0a2fad965870308cdd88106bef6fe430bb99aa
anas-mellouki <anas.mellouki@grenoble-inp.org>
2020-03-27 15:32:57 +0100

    ras

--- PricerValidation/Validation.cpp
+++ PricerValidation/Validation.cpp
@@ -5,10 +5,7 @@
 #include "QuantoValidation.hpp"
 
 
-void test_wrapper() {
-	int size = 3;
-	double r = 0.01;
-	int nb_dates = 1046;
+double test_fx() {
 	double s0_[] = { 100, 0.1, 100,0.2, 100 };
 	double trends_[] = { 0.01, 0.02, 0.03, 0.02, 0.04 };
 	double sigmas_[] = { 0.2, 0.25, 0.3, 0.3, 0.2 };
@@ -18,27 +15,51 @@ void test_wrapper() {
 		correlation[i + 5 * i] = 1;
 	}
 	int size_fx = 5;
-	PnlMat* path = pnl_mat_create(nb_dates + 1, size_fx);
-	PnlVect* s0 = pnl_vect_create_from_ptr(size_fx, s0_);
-	PnlVect* sigma = pnl_vect_create_from_ptr(size_fx, sigmas_);
-	PnlVect* trend_vec = pnl_vect_create_from_ptr(size_fx, trends_);
 	PnlMat* corr_mat = pnl_mat_create_from_ptr(size_fx, size_fx, correlation);
-	PnlVect* vect_r = pnl_vect_create_from_double(size, r);
-	pnl_vect_set(vect_r, 1, r);
-	pnl_vect_set(vect_r, 2, r);
-	FXBlackScholes* model = new FXBlackScholes(size_fx, vect_r, sigma, s0, trend_vec, corr_mat);
+	double t = 0;
+	double r = 0.01, r_US = 0.01, r_HK = 0.02;
+	double T = 8;
+	int n_time_steps = 16;
+	double netAssetValue = 100;
+	int nb_samples = 1000;
+	double sigma_[] = { 0.2,0.15,0.05,0.2,0.04 };
+	double S0_[] = { 1000,1500,0.9,2000,0.1 };
+	PnlVect* S0 = pnl_vect_create_from_ptr(size_fx, S0_);
+	PnlVect* vect_r = pnl_vect_create_from_double(size_fx, r);
+	pnl_vect_set(vect_r, 1, r_US);
+	pnl_vect_set(vect_r, 2, r_HK);
+	PnlVect* sigma = pnl_vect_create_from_ptr(size_fx, sigma_);
+	PnlVect* trend_vec = pnl_vect_create_from_double(size_fx, r);
+	FXBlackScholes* model = new FXBlackScholes(size_fx, vect_r, sigma, S0, trend_vec, corr_mat);
+	PnlVect* initial_values_ = pnl_vect_create_from_ptr(size_fx, S0_);
+	KanjiOptionFX* kanji_fx = new KanjiOptionFX(T, n_time_steps, size_fx, initial_values_, netAssetValue, r_US, r_HK);
 	PnlRng* rng = pnl_rng_create(PNL_RNG_MERSENNE);
 	pnl_rng_sseed(rng, time(NULL));
-	double maturity = 8.7896825396825395;
-	double t = 4.6428571428571432;
-	model->simul_market(path, maturity - t, nb_dates, rng);
-	pnl_mat_print(path);
-	double *path_ = new double[size_fx*nb_dates];
- 	for (int index_row = 0; index_row < path->m; index_row++) {
-		for (int index_col = 0; index_col < path->n; index_col++) {
-			path_[index_row * path->n + index_col] = pnl_mat_get(path, index_row, index_col);
-		}
-	}
+	MonteCarlo* mc = new MonteCarlo(model, kanji_fx, rng, T / n_time_steps, nb_samples, 0.07);
+	double prix = 0, ic = 0;
+	mc->price(prix, ic);
+	return prix;
+}
+
+double test() {
+	int n_time_steps = 16;
+	double T = 8;
+	double r = 0.0001;
+	double s0_[] = { 100, 0.1, 100,0.2, 100 };
+	double sigma_[] = { 0.2,0.15,0.2};
+	int size = 3;
+	PnlVect *spot = pnl_vect_create_from_ptr(size, s0_);
+	PnlVect *sigma = pnl_vect_create_from_ptr(size, sigma_);
+	PnlVect *trend = pnl_vect_create_from_scalar(size, 0.0002);
+	BlackScholesModel* model = new BlackScholesModel(size, r, 0.2, sigma, spot, trend);
+	KanjiOption *kanji = new KanjiOption(T, n_time_steps, size, spot, 100);
+	int n_samples = 1000;
+	PnlRng* rng = pnl_rng_create(PNL_RNG_MERSENNE);
+	pnl_rng_sseed(rng, time(NULL));
+	MonteCarlo* mc = new MonteCarlo(model, kanji, rng, T / n_time_steps, n_samples, 0.07);
+	double prix = 0, ic = 0;
+	mc->price(prix, ic);
+	return prix;
 }
 int main() {
 	// initializing the random number generator 
@@ -47,11 +68,10 @@ int main() {
 	//validate_call(rng);
 	//validate_kanji(rng);
 	//validate_quanto(rng);
-	validate_basket(rng);
+	//validate_basket(rng);
 	//validate_kanjiFX(rng);
 	//validate_hedging();
-	//test_wrapper();
-
-
+	//double prix = test();
+	//double prix_fx = test_fx();
 	return 0;
 }
--- PricingKanji/Test/TestTimeComputation.cs
+++ PricingKanji/Test/TestTimeComputation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Wrapper;
 using System.IO;
 using PricingKanji.Model;
 using PricingLibrary.Utilities.MarketDataFeed;
